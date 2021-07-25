1a468bfbb509b6f47710e2209bacc8f0e3bd1a63
DeograciousAggrey <DeograciousAggrey@gmail.com>
2021-07-25 12:35:35 +0300

    Updated the code files

--- Marketplace.sol
+++ Marketplace.sol
@@ -1,64 +1,74 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT 
+pragma solidity >=0.7.0 < 0.9.0;
 
-pragma solidity >=0.7.0 <0.9.0;
 
-contract Marketplace {
-
-    uint internal productsLength = 0;
-
-    struct Product {
-        address payable owner;
-        string name;
-        string image;
-        string description;
-        string location;
-        uint price;
-        uint sold;
-    }
-
-    mapping (uint => Product) internal products;
-
-    function writeProduct(
-        string memory _name,
-        string memory _image,
-        string memory _description, 
-        string memory _location, 
-        uint _price
-    ) public {
-        uint _sold = 0;
-        products[productsLength] = Product(
-            payable(msg.sender),
-            _name,
-            _image,
-            _description,
-            _location,
-            _price,
-            _sold
-        );
-        productsLength++;
-    }
-
-    function readProduct(uint _index) public view returns (
-        address payable,
-        string memory, 
-        string memory, 
-        string memory, 
-        string memory, 
-        uint, 
-        uint
-    ) {
-        return (
-            products[_index].owner,
-            products[_index].name, 
-            products[_index].image, 
-            products[_index].description, 
-            products[_index].location, 
-            products[_index].price,
-            products[_index].sold
-        );
+    interface IERC20Token {
+          function transfer(address, uint256) external returns (bool);
+          function approve(address, uint256) external returns (bool);
+          function transferFrom(address, address, uint256) external returns (bool);
+          function totalSupply() external view returns (uint256);
+          function balanceOf(address) external view returns (uint256);
+          function allowance(address, address) external view returns (uint256);
+        
+          event Transfer(address indexed from, address indexed to, uint256 value);
+          event Approval(address indexed owner, address indexed spender, uint256 value);
     }
     
-    function getProductsLength() public view returns (uint) {
-        return (productsLength);
-    }
+    contract Marketplace  {
+        
+        struct Product {
+            address payable owner;
+            string name;
+            string image;
+            string description;
+            string location;
+            uint price;
+            uint sold;
+        }
+        
+        Product[] public products;
+        mapping(uint => Product) internal productM;
+        
+        address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+        
+        
+        function addProduct(
+            string memory _name,
+            string memory _image,
+            string memory _description,
+            string memory _location,
+            uint _price 
+            
+            ) public {
+                uint _sold = 0;
+                for (uint i = 0; i< products.length; i++) {
+                    productM[i] = Product(
+                        payable(msg.sender),
+                        _name,
+                        _image,
+                        _description,
+                        _location,
+                        _price,
+                        _sold
+                        );
+                        products.push(productM[i]);
+                }
+        }
+        
+        function readProduct(uint  _index) public view returns (Product memory) {
+               
+                
+        }
+        
+        
+        function buyProduct(uint _index) public payable {
+            require(IERC20Token(cUsdTokenAddress).transferFrom(msg.sender,productM[_index].owner,productM[_index].price), "Transfer failed");
+            
+            productM[_index].sold++;
+        }
+        
+        function getProductLength() public view returns(uint){
+            return products.length;   
+        }
+        
 }
